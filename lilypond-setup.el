558bec09243b8e311d58586a6579ead2960ee764
Eric Frederickson <ericfrederickson68@gmail.com>
2023-04-19 00:17:24 -0500

    corrected a path

--- lilypond-setup.el
+++ lilypond-setup.el
@@ -4,7 +4,7 @@
 
 (if (string-equal myOs "macOs")
     (load-file "/opt/homebrew/share/emacs/site-lisp/lilypond/lilypond-init.el")
-  (load-file "/usr/local/share/emacs/site-lisp/lilypond-init.el"))
+  (load-file "/usr/share/emacs/site-lisp/lilypond-init.el"))
 
 (if (string-equal myOs "macOs")
     (setq load-path (append '("/opt/homebrew/share/emacs/site-lisp/lilypond") load-path)))
