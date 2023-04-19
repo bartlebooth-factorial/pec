17039e696855af170bc011c762a0e66d3122ff7b
Eric Frederickson <ericfrederickson68@gmail.com>
2023-04-19 00:29:31 -0500

    added a macOs guard to a Lilypond config

--- lilypond-setup.el
+++ lilypond-setup.el
@@ -9,7 +9,8 @@
 (if (string-equal myOs "macOs")
     (setq load-path (append '("/opt/homebrew/share/emacs/site-lisp/lilypond") load-path)))
 
-(eval-after-load 'flycheck '(require 'flycheck-lilypond))
+(if (string-equal myOs "macOs")
+    (eval-after-load 'flycheck '(require 'flycheck-lilypond)))
 
 (add-hook 'LilyPond-mode-hook (lambda ()
 				(flycheck-mode)))
