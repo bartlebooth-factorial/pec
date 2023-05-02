741b6f086f65da5268c761a303e86d1657062e9f
Eric Frederickson <ericfrederickson68@gmail.com>
2023-05-02 16:20:49 -0500

    added syntax highlighting for code blocks in markdown mode

--- markdown-setup.el
+++ markdown-setup.el
@@ -2,6 +2,8 @@
 ;;; markdown-setup.el -- Customizations for writing Markdown
 ;;;
 
+(setq markdown-fontify-code-blocks-natively t)
+
 (add-hook 'markdown-mode-hook (lambda ()
 				(auto-fill-mode 1)
 				(setq-local fill-column 80)))
