ff509a499eb13b3e3fe4c9709bc4287a002faca9
Eric Frederickson <ericfrederickson68@gmail.com>
2023-08-07 16:34:00 -0500

    changed plaintext capf to cape-dabbrev

--- plaintext-setup.el
+++ plaintext-setup.el
@@ -14,8 +14,7 @@
 (add-hook 'text-mode-hook (lambda ()
 			    (setq captain-predicate
 				  (lambda () t))
-			    (local-set-key (kbd "M-/") 'cape-ispell)
-			    (setq-local completion-at-point-functions 'cape-ispell)
+			    (setq-local completion-at-point-functions 'cape-dabbrev)
 			    (setq-local corfu-auto t)
 			    (setq-local corfu-auto-delay 0.2)
 			    (set-fill-column 80)
