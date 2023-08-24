9d7445e1de0581c53aa0a64917b66f7d8ac77ffc
Eric Frederickson <ericfrederickson68@gmail.com>
2023-08-24 12:59:55 -0500

    mail configs don't trigger on mac

--- mail-setup.el
+++ mail-setup.el
@@ -2,35 +2,37 @@
 ;;; package-setup.el -- Email config using notmuch.el
 ;;;
 
-;; for reading mail
-(use-package notmuch
-  :init
-  (setq notmuch-fcc-dirs nil
-	notmuch-hello-thousands-separator ","
-	notmuch-show-logo nil
-	notmuch-show-empty-saved-searches nil
-	notmuch-hello-recent-searches-max 5
-	notmuch-search-oldest-first nil)
-  (add-hook 'notmuch-search-mode-hook (lambda ()
-					(setq-local notmuch-search-oldest-first nil))))
+(ifmac nil
+       (progn
+	 ;; for reading mail
+	 (use-package notmuch
+	   :init
+	   (setq notmuch-fcc-dirs nil
+		 notmuch-hello-thousands-separator ","
+		 notmuch-show-logo nil
+		 notmuch-show-empty-saved-searches nil
+		 notmuch-hello-recent-searches-max 5
+		 notmuch-search-oldest-first nil)
+	   (add-hook 'notmuch-search-mode-hook (lambda ()
+						 (setq-local notmuch-search-oldest-first nil))))
 
-;; modeline indicator for number of unread messages
-(use-package notmuch-indicator
-  :init
-  (setq notmuch-indicator-args '((:terms "tag:unread and tag:inbox" :label "@"))
-	notmuch-indicator-refresh-count 10
-	notmuch-indicator-force-refresh-commands '(notmuch-refresh-this-buffer)
-	notmuch-indicator-hide-empty-counters nil)
-  :config
-  (notmuch-indicator-mode 1))
+	 ;; modeline indicator for number of unread messages
+	 (use-package notmuch-indicator
+	   :init
+	   (setq notmuch-indicator-args '((:terms "tag:unread and tag:inbox" :label "@"))
+		 notmuch-indicator-refresh-count 10
+		 notmuch-indicator-force-refresh-commands '(notmuch-refresh-this-buffer)
+		 notmuch-indicator-hide-empty-counters nil)
+	   :config
+	   (notmuch-indicator-mode 1))
 
-;; for sending mail
-(setq send-mail-function 'sendmail-send-it
-      sendmail-program (executable-find "msmtp")
-      mail-specify-envelope-from t
-      message-sendmail-envelope-from 'header
-      mail-envelope-from 'header)
+	 ;; for sending mail
+	 (setq send-mail-function 'sendmail-send-it
+	       sendmail-program (executable-find "msmtp")
+	       mail-specify-envelope-from t
+	       message-sendmail-envelope-from 'header
+	       mail-envelope-from 'header)
 
-(global-set-key (kbd "C-z m") (lambda ()
-				(interactive)
-				(notmuch-search-by-tag "inbox")))
+	 (global-set-key (kbd "C-z m") (lambda ()
+					 (interactive)
+					 (notmuch-search-by-tag "inbox")))))
