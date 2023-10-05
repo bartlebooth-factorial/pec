eb9bbb052767dd958b2f0a0acc8e58ee46cb931b
Eric Frederickson <ericfrederickson68@gmail.com>
2023-10-04 19:17:31 -0500

    c mode: new default style

--- c-family-setup.el
+++ c-family-setup.el
@@ -3,11 +3,8 @@
 ;;;
 
 (add-hook 'c-mode-common-hook (lambda ()
-				(c-set-style "bsd")
+				;; (c-set-style "bsd")
+				(c-set-style "stroustrup")
 				;; (lsp-mode)
 				;; (flyspell-prog-mode)
 				))
-
-(sp-local-pair 'c-mode "{" nil :post-handlers '(:add ("||\n[i]" "RET")))
-(sp-local-pair 'c++-mode "{" nil :post-handlers '(:add ("||\n[i]" "RET")))
-(sp-local-pair 'js-mode "{" nil :post-handlers '(:add ("||\n[i]" "RET")))
