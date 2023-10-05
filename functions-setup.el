1b8ca59bbc6e8a0a93633d82cd365b8859bf7362
Eric Frederickson <ericfrederickson68@gmail.com>
2023-10-04 19:12:16 -0500

    add incr- and decr-num-at-point functions, and copy-line function

--- functions-setup.el
+++ functions-setup.el
@@ -45,3 +45,49 @@
 	   (if arg
 	       (dired-other-window target-dir)
 	     (dired target-dir)))))
+
+(defun change-num-at-point (change-func)
+  "Replace the number under the point with (change-func number)"
+  (if (number-at-point)
+      (let ((new-num (funcall change-func (number-at-point))))
+	(backward-sexp)
+	(kill-sexp)
+	(insert (number-to-string new-num)))))
+
+(defun seek-to-num ()
+  "Move the point to the next number in the buffer"
+  (if (number-at-point)
+      t
+    (if (= (point) (buffer-end 1))
+	nil
+      (progn
+	(forward-word)
+	(seek-to-num)))))
+
+(defun seek-to-num-and-change (change-func)
+  "Go to next number in buffer and replace it with (change-func number)"
+  (push-mark)
+  (if (seek-to-num)
+      (change-num-at-point change-func)
+    (progn
+      (message "No number found in buffer after point"))))
+
+(defun incr-num-at-point ()
+  (interactive)
+  (seek-to-num-and-change (lambda (x) (+ x 1))))
+
+(defun decr-num-at-point ()
+  (interactive)
+  (seek-to-num-and-change (lambda (x) (- x 1))))
+
+
+
+(defun copy-line (&optional arg)
+  (interactive "P")
+  (save-mark-and-excursion
+    (unless arg
+      (back-to-indentation))
+    (push-mark)
+    (move-end-of-line 1)
+    (activate-mark)
+    (kill-ring-save nil nil t)))
