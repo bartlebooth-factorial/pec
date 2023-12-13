8263f9664ef2d0a974ecd438b4e9842bf5b34a98
Eric Frederickson <ericfrederickson68@gmail.com>
2023-12-13 11:51:38 -0600

    added some packages

--- package-setup-optm.el
+++ package-setup-optm.el
@@ -33,6 +33,27 @@
   :config
   (setq chess-images-default-size 70))
 
+(ifmac (use-package combobulate
+  :preface
+  ;; You can customize Combobulate's key prefix here.
+  ;; Note that you may have to restart Emacs for this to take effect!
+  (setq combobulate-key-prefix "C-c o")
+
+  ;; Optional, but recommended.
+  ;;
+  ;; You can manually enable Combobulate with `M-x
+  ;; combobulate-mode'.
+  :hook ((python-ts-mode . combobulate-mode)
+         (js-ts-mode . combobulate-mode)
+         (css-ts-mode . combobulate-mode)
+         (yaml-ts-mode . combobulate-mode)
+         (json-ts-mode . combobulate-mode)
+         (typescript-ts-mode . combobulate-mode)
+         (tsx-ts-mode . combobulate-mode))
+  ;; Amend this to the directory where you keep Combobulate's source
+  ;; code.
+  :load-path ("/Users/ericfrederickson/repos/combobulate")))
+
 (use-package consult
   :defer t
   :config
@@ -74,6 +95,10 @@
 (use-package expand-region
   :bind ("C-=" . er/expand-region))
 
+(use-package file-info)
+
+(use-package git-gutter-fringe)
+
 (use-package haskell-mode
   :defer t)
 
@@ -96,6 +121,9 @@
 	 ("C->" . mc/mark-next-like-this)
 	 ("C-<" . mc/mark-previous-like-this)
 	 ("C-c C->" . mc/mark-all-like-this)))
+
+(use-package origami
+  :config (global-origami-mode))
 
 (use-package pdf-tools
   :hook (doc-view-mode . pdf-tools-install))
