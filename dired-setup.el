cde6aaa640311ce12719675a2a63caa8bf1cd02c
Eric Frederickson <ericfrederickson68@gmail.com>
2024-11-28 17:57:40 -0600

    dired: bind ";" to system's default open file cmd

--- dired-setup.el
+++ dired-setup.el
@@ -2,10 +2,8 @@
 ;;; dired-setup.el -- configurations for Dired
 ;;;
 
-(ifmac (progn (defun dired-sysopen-file-at-point ()
-		(interactive)
-		(async-shell-command (concat "open " (dired-file-name-at-point)) nil nil))
+(defun dired-sysopen-file-at-point ()
+  (interactive)
+  (async-shell-command (concat "open " (dired-file-name-at-point)) nil nil))
 
-	      (use-package dired
-		:bind (:map dired-mode-map
-			    (";" . dired-sysopen-file-at-point)))))
+(keymap-set dired-mode-map (kbd ";") 'dired-sysopen-file-at-point))
