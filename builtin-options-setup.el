14315710d26e6f26f076450246df51d467832889
Eric Frederickson <ericfrederickson68@gmail.com>
2024-11-28 17:50:50 -0600

    builtin options: enable headings in grep buffers; set default buffer to scratch buffer; enable pruning native compilation cache; additions to exec-path

--- builtin-options-setup.el
+++ builtin-options-setup.el
@@ -13,8 +13,9 @@
 (setq eshell-hitory-size 1024)
 (setq gc-cons-threshold 20000000)
 (setq global-auto-revert-non-file-buffers t)
+(setq grep-use-headings t)
 (setq inhibit-startup-screen t)
-(setq initial-buffer-choice "~/csci/todo-csci.org")
+(setq initial-buffer-choice t)
 (setq initial-scratch-message ";; Scratch\n\n")
 (setq kept-new-versions 6)
 (setq kept-old-versions 2)
@@ -25,9 +26,17 @@
 (setq use-dialog-box nil)
 (setq version-control t)
 
-(ifmac nil
+;; (ifmac nil
  (progn
    (setq native-comp-async-report-warnings-errors 'silent)
-   (setq native-compile-prune-cache t)))
+   (setq native-compile-prune-cache t))
+;; )
 
 (add-to-list 'exec-path "/Library/TeX/texbin")
+(add-to-list 'exec-path "/Library/Frameworks/Python.framework/Versions/3.10/bin")
+(add-to-list 'exec-path "~/.ghcup/bin")
+(add-to-list 'exec-path "/usr/local/smlnj/bin")
+(add-to-list 'exec-path "~/.opam/default/bin")
+(add-to-list 'exec-path "/Users/ericfrederickson/.opam/CP.2023.03.0~8.17~2023.08/bin")
+(add-to-list 'exec-path "/opt/homebrew/bin/")
+(add-to-list 'exec-path "/opt/local/bin/")
