96bade711e9f915519641fcc54921f90a657cab1
Eric Frederickson <ericfrederickson68@gmail.com>
2024-11-28 18:01:58 -0600

    markdown: use adaptive-wrap package

--- markdown-setup.el
+++ markdown-setup.el
@@ -4,8 +4,13 @@
 
 (setq markdown-fontify-code-blocks-natively t)
 
+(use-package adaptive-wrap
+  :ensure t
+  :commands adaptive-wrap-prefix-mode)
+
 (add-hook 'markdown-mode-hook (lambda ()
 				(setq-local completion-at-point-functions 'cape-dabbrev)
 				(setq-local fill-column 80)
 				(auto-fill-mode 1)
-				(flyspell-mode 1)))
+				(flyspell-mode 1)
+				(adaptive-wrap-prefix-mode 1)))
