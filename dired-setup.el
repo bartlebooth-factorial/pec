1ee039c4bac5941450b28987c7c74d8ca967f81d
Eric Frederickson <ericfrederickson68@gmail.com>
2025-01-09 20:26:26 -0600

    refine Dired config

--- dired-setup.el
+++ dired-setup.el
@@ -4,8 +4,10 @@
 
 (defun dired-sysopen-file-at-point ()
   (interactive)
-  (async-shell-command (concat "open " (dired-file-name-at-point)) nil nil))
+  (async-shell-command (concat "open " (dired-file-name-at-point)) "open (from dired)"))
 
 (add-hook 'dired-mode-hook
 	  (lambda ()
 	    (keymap-set dired-mode-map (kbd ";") 'dired-sysopen-file-at-point)))
+
+(setq dired-listing-switches "-alh")
